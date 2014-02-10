d1e30c1a840f443bc20f5b57cdbf63e7a9ca8cba
Anton Khlynovskiy <subzey@gmail.com>
2014-02-11 00:59:52 +0400

    WIP

--- submission.js
+++ submission.js
@@ -1,30 +1,28 @@
-a.width = 64;
-a.height = 64;
+a.width = 99;
+a.height = 48;
 var values = [];
-var imageData = c.getImageData(0,0,64,64);
+var imageData = c.getImageData(0,0,99,48);
 var pixelData = imageData.data;
 setInterval(function(){
 	var newValues = [];
-	for (var i=64*64; i--; ){
-		var x = i % 64;
-		var yCoeff = 1 + x / 64 * 2;
+	for (var i=99*48; i--; ){
+		var x = i % 99;
+		var xCoeff = (x/128/3);
+		var yCoeff = 1 + x / 128 * 3;
 		newValues[i] = (
-			values[i] * 5 + // self
-			values[i+64] * yCoeff + // bottom
-			values[i-64] * (4 - yCoeff) + // top
-			values[i-1] * 3 // left
-		) / (11.5 + Math.random()) || 0;
-		if (x == 0 && i > 15 * 64 && i < 25 * 64){
+			values[i] * 3 + // self
+			values[i+99] * yCoeff + xCoeff * 2 + // bottom
+			values[i-99] * (5 - yCoeff) + xCoeff * 2 + // top
+			values[i-1] * (5 - xCoeff) // left
+		) / (12.4 + Math.random()) || 0;
+		if (x === 0 && i > 2 * 99 && i < 9 * 99){
 			if (!window.off){
 				newValues[i] = 255;
 			}
 		}
-		if (x == 63){
-			newValues[i] = 0;
-		}
 		pixelData[i*4] = newValues[i];
 		pixelData[i*4+1] = newValues[i] * 2 - 256;
-		pixelData[i*4+2] = 0;
+		pixelData[i*4+2] = i/(99*48)*60;
 		pixelData[i*4 + 3] = 255;
 	}
 	values = newValues;

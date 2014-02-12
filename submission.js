91a31a920b7147f24ee93964395fccc6e5e43c09
Anton Khlynovskiy <subzey@gmail.com>
2014-02-12 20:30:30 +0400

    Coeffs

--- submission.js
+++ submission.js
@@ -1,16 +1,15 @@
-a.width=123;a.height=48;
 V=[];
-I=c.getImageData(0,0,123,48);P=I.data;
+I=c.getImageData(0,0,a.width=150,a.height=60);P=I.data;
 setInterval(function(){
 	N=[];
-	for(i=123*48;i--;){
-		x=i%123;
-		X=x/512;
-		Y=1+x/64;
-		N[i]=(x==24&&i>13*123&&i<18*123)?255:(V[i]*3+V[i+123]*Y+X*2+V[i-123]*(5-Y)+X*2+V[i-1]*(5-X))/(12.4+Math.random())||0;
+	for(i=150*60;i--;){
+		x=i%150;
+		X=x/500;
+		Y=1+x/60;
+		N[i]=(x==30&&i>16*150&&i<24*150)?255:(V[i]*3+V[i+150]*Y+V[i-150]*(5-Y)+V[i-1]*(5-X))/(12.37+Math.random())||0;
 		P[i*4]=N[i];
 		P[i*4+1]=N[i]*2-256;
-		P[i*4+2]=i/(123*48)*60;
+		P[i*4+2]=i/150;
 		P[i*4+3]=255;
 	}
 	V=N;

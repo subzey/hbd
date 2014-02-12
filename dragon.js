01a3ae295c3760c14e5925e6a9c579ccd59f5f08
Anton Khlynovskiy <subzey@gmail.com>
2014-02-12 21:28:58 +0400

    WIP

--- dragon.js
+++ dragon.js
@@ -1,16 +1,15 @@
-b.bgColor=0;
-var act = true;
-
+A=b.bgColor=0;
+A=1;
 c.scale(a.width/200,a.height/200);
 c.beginPath();
 c.moveTo(52,56);
-c.strokeStyle=act?'#730':'#333';
+c.strokeStyle=A?'#730':'#333';
 c.bezierCurveTo(24,0,-50,-50,0,240);
 c.bezierCurveTo(84,120,0,120,24,56);
-c.stroke(c.fillStyle=act?'#310':'#100');
+c.stroke(c.fillStyle=A?'#310':'#000');
 c.fill();
 c.beginPath();
 c.moveTo(40,56);
 c.bezierCurveTo(52,116,-24,36,52,56);
-act&&(c.fillStyle="#FF0");
+A&&(c.fillStyle="#FF0");
 c.fill();
